6709ab06d194976e22dcd379a32515cc9dcdf936
Palita Humranan <palita.hum64@gmail.com>
2025-08-24 18:29:28 +0700

    locust-trigger

--- test_login.py
+++ test_login.py
@@ -1,28 +1,37 @@
+# import pytest
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 import pytest
+import logging
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
+# ตั้งค่า logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 @pytest.fixture
 def browser():
+    logging.info("เปิด Chrome Browser แบบ headless")
     options = Options()
-    options.add_argument("--headless=new")
+    options.add_argument("--headless=new")  
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(options=options)
     yield driver
+    logging.info("ปิด Browser")
     driver.quit()
 
-
-def test_login(browser):
-    browser.get("http://google.com")
-    WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.NAME, "q")) 
-    )
-    assert "Dashboard" in browser.page_source
+def test_open_google(browser):
+    logging.info("ไปที่เว็บ Google")
+    browser.get("https://www.google.com")
+    
+    logging.info("ตรวจสอบ title ของหน้าเว็บ")
+    assert "Google" in browser.title
+    logging.info("เปิดเว็บ Google สำเร็จ")
 
 
 # def test_login(browser):
